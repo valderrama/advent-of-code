6ad2e7d7054bdf8d5e0124ccc21f669228f32741
Alex Valderrama <925981+valderrama@users.noreply.github.com>
2021-12-02 13:53:33 -0800

    Updated template

--- 2021/template-dir/solution.go
+++ 2021/template-dir/solution.go
@@ -12,16 +12,21 @@ const (
 )
 
 func main() {
+	// Variables
+
+	// Open file
 	file := file.Open(inputFile)
 	defer file.Close()
 
+	// For each line
 	scanner := bufio.NewScanner(file)
 	for i := 0; scanner.Scan(); i++ {
 		line := scanner.Text()
 		log.Print(line)
 	}
 
+	// Verify no I/O error
 	if err := scanner.Err(); err != nil {
-		log.Fatal("error while scanning:", err)
+		log.Fatal("error while scanning: ", err)
 	}
 }
